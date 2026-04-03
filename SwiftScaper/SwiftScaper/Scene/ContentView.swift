dbe66ec02fb539c851d29478d710710858bc7740
Alex Skorulis <skorulis@gmail.com>
2026-04-03 12:44:36 +1100

    Create Scraper using Swift

---
+++ SwiftScaper/SwiftScaper/Scene/ContentView.swift
@@ -0,0 +1,19 @@
+//  Created by Alexander Skorulis on 3/4/2026.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftScaper/SwiftScaper/Scene/SwiftScaperApp.swift
@@ -0,0 +1,12 @@
+//  Created by Alexander Skorulis on 3/4/2026.
+
+import SwiftUI
+
+@main
+struct SwiftScaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
